3fed7fdbfbf7daa26df117f0c669b7d9a2ad22d4
Jason Pickens <jasonpickensnz@gmail.com>
2024-02-12 16:23:33 +1300

    Replace ReactDOM.render

--- examples/todo/src/app.tsx
+++ examples/todo/src/app.tsx
@@ -1,6 +1,5 @@
 import 'todomvc-common';
 import * as React from 'react';
-import ReactDOM from 'react-dom';
 import {RelayEnvironmentProvider} from 'react-relay';
 import type {GraphQLResponse, RequestParameters, Variables} from 'relay-runtime';
 import {Environment, Network, RecordSource, Store} from 'relay-runtime';
@@ -8,6 +7,7 @@ import {ErrorBoundary} from 'react-error-boundary';
 import TodoAppEntryPoint from './entrypoints/TodoApp.entrypoint';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import {EntryPointRouteObject, preparePreloadableRoutes} from '@loop-payments/react-router-relay';
+import {createRoot} from 'react-dom/client';
 
 async function fetchQuery(params: RequestParameters, variables: Variables): Promise<GraphQLResponse> {
   const response = await fetch('/graphql', {
@@ -29,6 +29,7 @@ const modernEnvironment: Environment = new Environment({
 });
 
 const rootElement = document.getElementById('root')!;
+const root = createRoot(rootElement);
 
 const appEntryPoint: EntryPointRouteObject = {
   path: '/',
@@ -52,12 +53,12 @@ const routes = preparePreloadableRoutes(
 
 const router = createBrowserRouter(routes);
 
-ReactDOM.render(
+root.render(
   <RelayEnvironmentProvider environment={modernEnvironment}>
     <React.Suspense fallback={<div>Loading</div>}>
       <ErrorBoundary fallbackRender={({error}) => <div>{error.message}</div>}>
         <RouterProvider router={router} />
       </ErrorBoundary>
     </React.Suspense>
-  </RelayEnvironmentProvider>,
-  rootElement);
+  </RelayEnvironmentProvider>
+);
