8e9d0fc052706d035d31383647ada619c3a68084
Sean Nicolay <sean@loop.com>
2025-04-02 10:47:26 -0700

    Make preload hooks more dynamic

--- src/useLinkEntryPointLoadHandler.ts
+++ src/useLinkEntryPointLoadHandler.ts
@@ -4,7 +4,11 @@ import {
   isValidElement,
   type ComponentType,
 } from "react";
-import { type To, UNSAFE_DataRouterContext, matchRoutes } from "react-router-dom";
+import {
+  type To,
+  UNSAFE_DataRouterContext,
+  matchRoutes,
+} from "react-router-dom";
 import { InternalPreload } from "./routes/internal-preload-symbol";
 import type { PreloadableComponent } from "./routes/EntryPointRoute";
 
@@ -13,53 +17,70 @@ import type { PreloadableComponent } from "./routes/EntryPointRoute";
  * by Link to preload entrypoints on render. You
  * can use this to build your own Link component if necessary.
  */
-export function useLinkEntryPointLoadHandler(to: To): () => void {
+export function useLinkEntryPointLoadHandler(): (to: To) => void;
+/** @deprecated move the `to` argument to the callback */
+export function useLinkEntryPointLoadHandler(to: To): () => void;
+export function useLinkEntryPointLoadHandler(
+  deprecatedTo?: To
+): (to?: To) => void {
   const routes = useContext(UNSAFE_DataRouterContext)?.router.routes ?? [];
 
   // Fetch the entrypoint
-  const fetchEntrypoint = useCallback(() => {
-    const matches = matchRoutes([...routes], to);
-    if (!matches) {
-      return;
-    }
-    for (const match of matches) {
-      const route: any = match.route;
-      const { Component, element } = route;
-      let maybePreloadableComponent:
-        | ComponentType
-        | PreloadableComponent
-        | undefined;
-      if (Component) {
-        maybePreloadableComponent = Component;
-      } else if (isValidElement(element) && typeof element.type !== "string") {
-        maybePreloadableComponent = element.type;
+  const fetchEntrypoint = useCallback(
+    (to: To | undefined) => {
+      to ??= deprecatedTo;
+      // This shouldn't happen, at least one of to or deprecatedTo should be
+      // defined.
+      if (to == null) {
+        return;
       }
+      const matches = matchRoutes([...routes], to);
+      if (!matches) {
+        return;
+      }
+      for (const match of matches) {
+        const route: any = match.route;
+        const { Component, element } = route;
+        let maybePreloadableComponent:
+          | ComponentType
+          | PreloadableComponent
+          | undefined;
+        if (Component) {
+          maybePreloadableComponent = Component;
+        } else if (
+          isValidElement(element) &&
+          typeof element.type !== "string"
+        ) {
+          maybePreloadableComponent = element.type;
+        }
 
-      if (maybePreloadableComponent) {
-        try {
-          if (InternalPreload in maybePreloadableComponent) {
-            maybePreloadableComponent[InternalPreload]?.entryPoint().catch(
-              (e: unknown) => {
-                console.warn(
-                  `[react-router-relay] failed to preload ${
-                    match.pathname
-                  } entrypoint for route ${JSON.stringify(to)}`,
-                  e,
-                );
-              },
+        if (maybePreloadableComponent) {
+          try {
+            if (InternalPreload in maybePreloadableComponent) {
+              maybePreloadableComponent[InternalPreload]?.entryPoint().catch(
+                (e: unknown) => {
+                  console.warn(
+                    `[react-router-relay] failed to preload ${
+                      match.pathname
+                    } entrypoint for route ${JSON.stringify(to)}`,
+                    e
+                  );
+                }
+              );
+            }
+          } catch (e: unknown) {
+            console.warn(
+              `[react-router-relay] failed to call entrypoint preloader ${
+                match.pathname
+              } for route ${JSON.stringify(to)}`,
+              e
             );
           }
-        } catch (e: unknown) {
-          console.warn(
-            `[react-router-relay] failed to call entrypoint preloader ${
-              match.pathname
-            } for route ${JSON.stringify(to)}`,
-            e,
-          );
         }
       }
-    }
-  }, [routes, to]);
+    },
+    [routes, deprecatedTo]
+  );
 
   return fetchEntrypoint;
 }
